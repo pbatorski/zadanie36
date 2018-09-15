f012015cc9a7a24bd05f26a31f05914a2bf72af6
przemour@gmail.com <przemour@gmail.com>
2018-09-15 15:10:00 +0200

    zadanie 36 wykonane

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var num1 = n,
+    num2 = n2;
+
+function test (num1, num2) {
+    if (n === 20  || n2 === 20 || n + n2 <= 20) {
+        console.log("true");
+    }
+    else {
+        console.log("false");
+    }
+}
